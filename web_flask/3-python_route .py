e70b0a490f0f932a9ff9cef64c05cccb65fc233c
Mvke-IT <michael.duarte09@gmail.com>
2024-04-11 15:04:07 -0400

    going live

--- web_flask/3-python_route .py
+++ web_flask/3-python_route .py
@@ -1,40 +1,26 @@
 #!/usr/bin/python3
-"""Start web application with two routings
-"""
-
 from flask import Flask
+
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello():
-    """Return string when route queried
-    """
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
     return 'Hello HBNB!'
 
-
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Return string when route queried
-    """
     return 'HBNB'
 
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
-@app.route('/c/<text>')
-def c_is_fun(text):
-    """Return reformatted text
-    """
-    return 'C ' + text.replace('_', ' ')
-
-
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_with_text(text='is cool'):
-    """Reformat text based on optional variable
-    """
-    return 'Python ' + text.replace('_', ' ')
-
+@app.route('/python', defaults={'text': "is cool"}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py(text):
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
