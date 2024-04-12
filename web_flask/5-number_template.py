48e2522df3ebe115ccc5330fef3b96110410a857
Mvke-IT <michael.duarte09@gmail.com>
2024-04-12 17:53:35 -0400

    displaying an html page and other route for the template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,36 +1,49 @@
 #!/usr/bin/python3
+"""Start web application with two routings."""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
+
+@app.route('/')
+def hello():
+    """Return string when route queried."""
     return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
+
+@app.route('/hbnb')
 def hbnb():
+    """Return string when route queried."""
     return 'HBNB'
 
-@app.route('/c/<text>', strict_slashes=False)
-def text(text):
-    text = text.replace("_", " ")
-    return "C {}".format(text)
 
-@app.route('/python', defaults={'text': "is cool"}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def py(text):
-    text = text.replace('_', ' ')
-    return "Python {}".format(text)
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """Return reformatted text."""
+    return 'C ' + text.replace('_', ' ')
 
-@app.route('/number/<n>', strict_slashes=False)
-def numbers(n):
-    if n.isdigit():
-        return '{} is a number'.format(n)
 
-@app.route('/number_template/<n>', strict_slashes=False)
-def temp(n):
-    if n.isdigit():
-        return render_template('5-number.html', n=n)
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_with_text(text='is cool'):
+    """Reformat text based on optional variable."""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>')
+def number(n=None):
+    """Allow request if path variable is a valid integer."""
+    return str(n) + ' is a number'
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """Retrieve template for request."""
+    path = '5-number.html'
+    return render_template(path, n=n)
+
 
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
