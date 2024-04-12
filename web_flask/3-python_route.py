99685c29e5b8ae028a9b66cf9bff287c6088c081
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-04-12 10:27:15 -0400

    Flask framework tasks

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,14 +22,14 @@ def hbnb():
 app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
         text = unquote_plus(text.replace('_', ' '))
-        return (f"C {text}")
+        return f"C {text}"
 
 
 app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
 app.route('/python/<text>', strict_slashes=False)
 def python_with_text(text):
         text = unquote_plus(text.replace('_', ' '))
-        return (f"Python {text}")
+        return f"Python {text}"
 
 
 if __name__ == '__main__':
