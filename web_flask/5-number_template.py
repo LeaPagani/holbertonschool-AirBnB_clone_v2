3dad15cb1b08f251a3a70e41c44700573fb53842
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-04-12 22:12:21 -0400

    fixing task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,46 +1,52 @@
 #!/usr/bin/python3
-""" starts the flask app """
+""" This module starts a Flask web application """""
 from flask import Flask
 from flask import render_template
-
+""" Flask class and render_template method"""""
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+""" App route section
+    Add a second route /hbnb:
+    display “HBNB”
+
+    Add a third route /c/<text>:
+    display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space ) "
+
+    Add a fourth route /python/(<text>):
+    display “Python ”, followed by the value of the text variable"""""
 
 
-@app.route('/', strict_slashes=False)
-def hello_world():
-    """ returns hello HBNB """
+@app.route('/')
+def hello():
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    """ returns HBNB """
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def funC(text):
-    """ returns C """
-    return 'C ' + text.replace('_', ' ')
+@app.route('/c/<text>')
+def c(text):
+    return f"C {text.replace('_', ' ')}"
 
 
-@app.route('/python/', defaults={'text': "is cool"}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def pyfun(text):
-    """ returns python """
-    return 'Python ' + text.replace('_', ' ')
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python(text='is cool'):
+    return f"Python {text.replace('_', ' ')}"
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def its_a_num(n):
-    """ returns the number """
-    return str(n) + " is a number"
+@app.route('/number/<int:n>')
+def number(n):
+    return f"{n} is a number"
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>')
 def number_template(n):
-    """ display html only if n is an integer """
-    return render_template("5-number.html", n=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
