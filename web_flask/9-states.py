b0729871d2ba6900cc931b4e398fe37ee10aaf8b
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-04-12 22:03:21 -0400

    task 9

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
-"""Start web application with two routings
-"""
-
+""" starts a web flask app """
 from models import storage
-from models.state import State
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
-@app.route('/states')
-@app.route('/states/<id>')
-def states_list(id=None):
-    """Render template with states
-    """
-    path = '9-states.html'
-    states = storage.all(State)
-    return render_template(path, states=states, id=id)
+@app.route("/states", strict_slashes=False)
+def states():
+    """ display html page with all states """
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """ html with state id """
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session
-    """
+def teardown(exc):
+    """ remove the SQLA session """
     storage.close()
 
 
-if __name__ == '__main__':
-    app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
