29837e317c54a372c53ae8606ed0a32b74fe915a
Mvke-IT <michael.duarte09@gmail.com>
2024-05-30 13:23:47 -0400

    testing connection

--- web_flask/3-python_route .py
+++ web_flask/3-python_route .py
@@ -1,37 +1,34 @@
 #!/usr/bin/python3
-
-"""Start web application with two routings."""
-
+""" starts the flask app """
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    """Return string when route queried."""
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ returns hello HBNB """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Return string when route queried."""
+    """ returns HBNB """
     return 'HBNB'
 
 
-@app.route('/c/<text>')
-def c_is_fun(text):
-    """Return reformatted text."""
+@app.route('/c/<text>', strict_slashes=False)
+def funC(text):
+    """ returns C """
     return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_with_text(text='is cool'):
-    """Reformat text based on optional variable."""
+@app.route('/python/', defaults={'text': "is cool"}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pyfun(text):
+    """ returns python """
     return 'Python ' + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
